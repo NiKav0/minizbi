3517518c96ca0d5bd41d8fc2c7393517df8271b8
NiKav0 <105223490+NiKav0@users.noreply.github.com>
2024-01-19 08:55:47 +0100

    Add files via upload

--- minishell-master/ms_split.c
+++ minishell-master/ms_split.c
@@ -5,156 +5,120 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: calmouht <calmouht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/14 07:18:59 by calmouht          #+#    #+#             */
-/*   Updated: 2024/01/18 08:21:18 by calmouht         ###   ########.fr       */
+/*   Created: 2024/01/19 07:07:30 by calmouht          #+#    #+#             */
+/*   Updated: 2024/01/19 07:10:57 by calmouht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include "minishell.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 
-int is_w_space(char c)
+static int	skip_space(char *str)
 {
-    return (c == ' ' || c == '\t');
-}
-
-int is_s_char(char c) 
-{
-    return (c == '|' || c == '>' || c == '<' || c == '\'' || c == '\"' || c == '$');
-}
-
-int count(char *str)
-{
-    int i;
-    int counter;
-    char quote_mark;;
-
-    i = 0;
-    counter = 0;
-    while (str[i])
-    {
-        while (str[i] && is_w_space(str[i]))
-            i++;
-        if (str[i] == '\'' || str[i] == '\"')
-        {
-            quote_mark = str[i++];
-            while (str[i] && str[i] != quote_mark)
-            {
-                counter++;
-                i++;
-            }
-            if (str[i] == quote_mark)
-                i++;
-        }
-        else
-        {
-            while (str[i] && !is_w_space(str[i]) && !is_s_char(str[i]))
-                i++;
-        }
-        counter++;
-    }
-    return (counter);
-}
-
-char *allocate(char *str)
-{
-    int i;
-    char quote_mark;
-    
-    i = 0;
-    if (str[i] == '\'' || str[i] == '\"')
-    {
-         quote_mark = str[i++];
-         while (str[i] && str[i] != quote_mark)
-             i++;
-         if (str[i] == quote_mark)
-             i++;
-     }
-     else if (is_s_char(str[i]))
-            i++;
-     else
-     {
-         while (str[i] && !is_w_space(str[i]) && !is_s_char(str[i]))
-             i++;
-     }
-     char *word = (char *)malloc(sizeof(char) * (i + 1));
-     if (!word)
-         return NULL;
-     i = 0;
-     if (str[i] == '\'' || str[i] == '\"')
-     {
-         char quote_mark = str[i++];
-         while (str[i] && str[i] != quote_mark)
-         {
-             word[i - 1] = str[i];
-             i++;
-         }
-         if (str[i] == quote_mark)
-             i++;
-     }
-     else if (is_s_char(str[i]))
-        word[i++] = str[i];
-     else
-     {
-         while (str[i] && !is_w_space(str[i]) && !is_s_char(str[i]))
-         {
-             word[i] = str[i];
-             i++;
-         }
-     }
-     word[i] = '\0';
-     return word;
-}
-
-char **ms_split(char *cmd)
-{
-    int i;
-    int j;
-    int len;
-    char **ptr;
+	int		i;
+	int		opmax;
+	int		counter;
+	char	quote_mark;
 
 	i = 0;
-	j = 0;
-	len = count(cmd);
-	ptr = (char **)malloc(sizeof(char *) * (len + 1));
-    if (!ptr)
-        return NULL;
-    while (cmd[i])
-	{
-        while (cmd[i] && is_w_space(cmd[i]))
-            i++;
-        ptr[j] = allocate(&cmd[i]);
-        if (!ptr[j])
-        {
-            while (j > 0)
-                free(ptr[--j]);
-            free(ptr);
-            return NULL;
-        }
-        j++;
-        while (cmd[i] && !is_w_space(cmd[i]) && !is_s_char(cmd[i]))
-            i++;
-    }
-    ptr[len] = NULL;
-    return (ptr);
+	quote_mark = str[i];
+	opmax = 2;
+	counter = 0;
+	while (str[i] == quote_mark && str[i] != '\0' && counter++ < opmax)
+		i++;
+	return (i);
 }
 
-// TODO :
-// handle more than one redirection `echo >>> zebi`
-// handle opposite redirections >><
-// mhm double everything osf zebi hoa lmo9adas:w
-// handle double quotes
-
-int	main()
+static int	ft_length(char *str)
 {
-	char *arr = "  w e h>hh";
+	char	quote_mark;
+	int		i;
+
+	i = 0;
+	if (str[i] == '>' || str[i] == '<' || str[i] == '|')
+		i += skip_space(str);
+	else
+	{
+		while (str[i] != ' ' && str[i] != '\t' && str[i] != '\0' && str[i] != '>'
+			&& str[i] != '<' && str[i] != '|')
+		{
+			if (str[i] == '\"' || str[i] == '\'')
+			{
+				quote_mark = str[i++];
+				while (str[i] != quote_mark && str[i] != '\0')
+					i++;
+			}
+			i++;
+		}
+	}
+	return (i);
+}
+
+static int	ft_count(char *str)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] == ' ' || str[i] == '\t')
+			i++;
+		if (str[i] != '\0')
+			count++;
+		i += ft_length(&str[i]);
+	}
+	return (count);
+}
+
+char	**ms_split(char *cmd)
+{
+	char	**splitted;
+	int		j;
+	int		i;
+	int		len;
+
+	splitted = (char **)calloc((ft_count(cmd) + 1), sizeof(char *));
+	i = 0;
+	j = 0;
+	while (cmd[i] != '\0')
+	{
+		while ((cmd[i] == ' ' || cmd[i] == '\t') && cmd[i] != '\0')
+			i++;
+		if (cmd[i] == '\0')
+			break ;
+		len = ft_length(&cmd[i]);
+		splitted[j] = (char *)malloc(len + 1);
+		if (!splitted[j])
+		{
+			while (j > 0)
+			{
+				free(splitted[--j]);
+			}
+			free(splitted);
+			return (NULL);
+		}
+		strncpy(splitted[j++], &cmd[i], len);
+		splitted[j - 1][len] = '\0';
+		i += len;
+	}
+	return (splitted);
+}
+
+int	main(void)
+{
+	char *arr = "mohammed > ><> >>> ll\"ah wer|ew\"  e |\trw ";
 	char **ptr = ms_split(arr);
 	int i = 0;
-	while(ptr[i])
+	while (ptr[i])
 	{
 		printf("%s\n", ptr[i]);
+		free(ptr[i]);
 		i++;
 	}
+	free(ptr);
+	return (0);
 }
