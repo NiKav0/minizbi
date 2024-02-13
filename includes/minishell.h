9660589918276da29eb380dfb0ac2fe99944d397
NiKav0 <dimitariyc@gmail.com>
2024-02-13 02:22:19 +0100

    omfg

--- includes/minishell.h
+++ includes/minishell.h
@@ -7,4 +7,32 @@
 #include <readline/history.h>
 #include <stdbool.h>
 
+typedef struct	ms_cmd
+{
+	char **cmd;
+	e_type	input;
+	char	*infile;
+	e_type	output;
+	char *outfile;
+	struct s_cmd *next;
+}t_cmd;
+
+enum
+{
+	NONE,
+	HEREDOC,	//<<
+	LREDIR,		// <
+ 	RREDIR,		// >
+ 	APPEND,		// >>
+}e_type;
+
+typedef struct ms_env
+{
+	char *varname;
+	char *value;
+	struct t_env *next;
+}t_env;
+
+
+
 #endif
