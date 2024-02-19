45e496e765b3438c05880e62cf866d9b8116b384
NiKav0 <dimitariyc@gmail.com>
2024-02-19 03:26:59 +0100

    minishell init

---
+++ src/ms_cmdrender.c
@@ -0,0 +1,5 @@
+#include "../includes/minishell.h"
+
+ms_cmd *ms_rendercmd(char **command){
+	
+}
--- src/utility.c
+++ src/utility.c
@@ -1,18 +1,47 @@
 #include "../includes/minishell.h"
 #include <stdio.h>
 
+e_type ms_ctrlop(char *str)
+{
+	if (ft_strcmp(str, "<<") == 0)
+		return (HEREDOC);
+	else if (ft_strcmp(str, "<") == 0)
+		return (LREDIR);
+	else if (ft_strcmp(str, ">") == 0)
+		return (RREDIR);
+	else if (ft_strcmp(str, ">>") == 0)
+		return (APPEND);
+	else
+		return (NONE);
+}
+
+char **ft_arrslice(char **arr, int start, int end)
+{
+	char **ptr;
+	int i;
+
+	i = 0;
+	ptr = (char **)malloc(sizeof(char *) * (end - start + 1));
+	while (start < end)
+	{
+		ptr[i] = ft_strdup(arr[start]);
+		start++;
+		i++;
+	}
+	ptr[i] = NULL;
+	return (ptr);
+}
+
 char *ft_strreplace(char *src, char *dst, char *replacement)
 {
 	char *ptr;
-
-
+	
 		if (strstr(src, dst))
 		{
 			ptr = malloc(sizeof(char) * (ft_strlen(src) - ft_strlen(dst) + ft_strlen(replacement) + 1));
 			ft_strlcpy(ptr, src, strstr(src, dst) - src + 1);
 			ft_strlcat(ptr, replacement, ft_strlen(ptr) + ft_strlen(replacement) + 1);
 			ft_strlcat(ptr, strstr(src,dst)+ ft_strlen(dst), ft_strlen(ptr) + ft_strlen(strstr(src,dst)+ ft_strlen(dst)) + 1);
-			src++;
 		}
 		else
 			ptr = ft_strdup(src);
